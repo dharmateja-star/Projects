3fab81e4cfc16a881e5034cf164708c85b5ae83a
dharmateja-star <tej.modu@gmail.com>
2025-03-18 17:12:55 +0530

    For,While,DoWhile & Patterns

---
+++ 1.Loops & Patterns/For_While_DoWhile.java
@@ -0,0 +1,34 @@
+ 
+class For_While_DoWhile
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("For Loop");
+		for(int i=0;i<=4;i++)
+		{
+			System.out.println("*");
+			
+		}
+
+
+		System.out.println("While Loop");
+		int j=0;
+		while(j<5)
+		{
+			System.out.println("*");
+			j++;
+		}
+
+
+		System.out.println("DoWhile Loop");
+		int k=5;
+		do{
+			System.out.println("*");
+			k++;
+		}while(k<10);
+
+		
+		
+		
+	}
+}
